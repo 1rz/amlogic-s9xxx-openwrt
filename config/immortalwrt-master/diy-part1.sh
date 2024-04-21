333d5f5174a6d5a6fee38555c2d4cf2ecde7c247
ZeroFirst <64071276+1rz@users.noreply.github.com>
2024-04-21 17:51:00 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -12,3 +12,10 @@
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-theme-argon-config
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
+sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
